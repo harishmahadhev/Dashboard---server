5790b8d88d7a4bd033ef41cd7a5ae7ef404a53be
harishmahadhev <harishmahadhev@gmail.com>
2021-07-24 13:13:08 +0530

    ..

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { loginRouter } from "./routes/login_route.js";
 import { userRouter } from "./routes/user_route.js";
 import cors from "cors";
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(json());
